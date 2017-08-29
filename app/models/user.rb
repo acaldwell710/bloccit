beaaaf9c419aaa983771b23205bec2687716da07
Amber Caldwell <acaldwell710@gmail.com>
2017-08-29 01:03:21 -0400

    added authentication to name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase if email.present? }
+  before_save :format_name
 
 # #3
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
@@ -14,4 +15,12 @@ class User < ApplicationRecord
 
 # #6
   has_secure_password
+
+  def format_name
+    name_array = []
+    name.split.each do |letter|
+      name_array << letter.capitalize
+    end
+    self.name = name_array.join(" ")
+  end
 end
