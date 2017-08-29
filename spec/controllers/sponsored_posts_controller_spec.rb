9ea8435f4874112d094bb80f91300aaeee415e6e
Amber Caldwell <acaldwell710@gmail.com>
2017-08-29 09:30:54 -0400

    updated sponsored posts

--- spec/controllers/sponsored_posts_controller_spec.rb
+++ spec/controllers/sponsored_posts_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe SponsoredPostsController, type: :controller do
 
   let(:my_topic) { Topic.create!(name:  RandomData.random_sentence, description: RandomData.random_paragraph) }
-  let(:my_sponsored_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+  let(:my_sponsored_post) { my_topic.sponsored_posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
 
   describe "GET new" do
@@ -98,7 +98,7 @@ RSpec.describe SponsoredPostsController, type: :controller do
        new_title = RandomData.random_sentence
        new_body = RandomData.random_paragraph
 
-       put :update, params: {topic_id: my_topic.id, id: my_sponsored_post.id, post: {title: new_title, body: new_body}}
+       put :update, params: {topic_id: my_topic.id, id: my_sponsored_post.id, sponsored_post: {title: new_title, body: new_body}}
  # #30
        expect(response).to redirect_to [my_topic, my_sponsored_post]
      end
