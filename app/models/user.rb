cb98a03f41bc78941814351f15ac7cc8d229630c
Amber Caldwell <acaldwell710@gmail.com>
2017-09-12 18:01:37 -0400

    added authentication of user groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 has_many :posts
 
   before_save { self.email = email.downcase if email.present? }
+  before_save { self.role ||= :member }
 
 # #3
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
@@ -16,4 +17,6 @@ has_many :posts
 
 # #6
   has_secure_password
+
+  enum role: [:member, :admin]
 end
